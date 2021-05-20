e8340d044fb106ebdacc723bcf3e761bb8dbf13f
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-20 20:37:17 +0530

    Added TabbedView (BottomNavigationBar), changed AppBar Background to White

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class UnitConverter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.indigo, accentColor: Colors.pink),
+      theme: ThemeData(primarySwatch: Colors.indigo),
       home: TabbedView(),
     );
   }
--- lib/tabbed_view.dart
+++ lib/tabbed_view.dart
@@ -1,10 +1,86 @@
 import 'package:flutter/material.dart';
 
-class TabbedView extends StatelessWidget {
+class TabbedView extends StatefulWidget {
+  @override
+  _TabbedViewState createState() => _TabbedViewState();
+}
+
+class _TabbedViewState extends State<TabbedView> {
+  var index = 0;
+
+  List<Widget> items = [
+    Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Length",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center(child: Text("Length View"))),
+    Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Volume",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center(child: Text("Volume View"))),
+    Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Temperature",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center(child: Text("Temperature View"))),
+    Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Time",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center(child: Text("Time View"))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Unit Converter")),
+      body: items[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (val) => setState(() => index = val),
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            label: "Length",
+            icon: Icon(Icons.rule),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            label: "Volume",
+            icon: Icon(Icons.rule),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            label: "Temperature",
+            icon: Icon(Icons.rule),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            label: "Time",
+            icon: Icon(Icons.rule),
+          ),
+        ],
+      ),
     );
   }
 }
