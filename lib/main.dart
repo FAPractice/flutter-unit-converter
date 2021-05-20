7977bc87a1388da1bb3daca1ecd36ee519e8ee3d
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-20 19:49:14 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:unit_converter/tabbed_view.dart';
+
+void main() {
+  runApp(UnitConverter());
+}
+
+class UnitConverter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.indigo, accentColor: Colors.pink),
+      home: TabbedView(),
+    );
+  }
+}
