c17d37438b2fc479845f4445c003778b1c0e0b95
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-21 16:12:00 +0530

    Added Copy to Clipboard on Tapping Result displayed in ListItemView

--- lib/Views/Supporting Views/display_values.dart
+++ lib/Views/Supporting Views/display_values.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DisplayValues extends StatelessWidget {
   final String heading, body;
@@ -8,6 +9,11 @@ class DisplayValues extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: () {
+        Clipboard.setData(ClipboardData(text: body));
+        final snackBar = SnackBar(content: Text('Copied to Clipboard'));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
       title: Text(heading, style: Theme.of(context).textTheme.subtitle1),
       subtitle: Text(body, style: Theme.of(context).textTheme.bodyText2),
     );
