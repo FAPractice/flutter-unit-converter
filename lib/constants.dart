eeef31d10670b170b420b7e62f90f980632bbd12
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-21 15:05:17 +0530

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kThemeTextInputStyle = InputDecoration(
+  hintText: "Enter Value",
+  border: InputBorder.none,
+  contentPadding: kListItemPadding,
+);
+
+const kListItemPadding =
+    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0);
